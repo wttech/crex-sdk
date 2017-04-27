3952580efaa23f9ef08d80f85fda207ef8362356
Mateusz Luczak <mateusz.luczak@cognifide.com>
2017-04-27 10:58:02 +0200

    Build update

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,21 @@
 var commonjs = require('rollup-plugin-commonjs');
+var resolve = require('rollup-plugin-node-resolve');
+var uglify = require('rollup-plugin-uglify');
 
 module.exports = {
 	entry: 'lib/index.js',
-	dest: 'dist/crex-sdk.js',
-	format: 'umd',
+	dest: 'dist/crex-sdk.min.js',
+	format: 'iife',
 	moduleName: 'CrEx',
 	plugins: [
+		resolve({
+			jsnext: true,
+			main: true,
+			browser: true
+		}),
 		commonjs({
 			sourceMap: false
-		})
+		}),
+		uglify()
 	]
 };
